698e38f7e9d037cfbd452405eb50cada507c0b99
zvdas <rodriguezzude@gmail.com>
2023-01-03 09:22:27 -0500

    react-redux project updated

---
+++ react-redux-master/src/components/MyButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import increment from '../action';
+
+const MyButton = () => {
+    let dispatch = useDispatch();
+    
+    return 
+    (
+        <button onClick={()=>dispatch(increment(1))}>Increase Counter</button>
+    );
+};
+
+export default MyButton;
